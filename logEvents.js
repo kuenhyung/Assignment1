0a28dfdcd036d276781183221a94e18db36f8580
kuenhyung <rhrmsgudahs@naver.com>
2026-03-22 21:22:09 +0900

    add consoleLog myName

--- logEvents.js
+++ logEvents.js
@@ -3,7 +3,7 @@ const { v4: uuid } = require('uuid');
 
 const fs = require('fs');
 const fsPromises = require('fs').promises;
-const path = require('path');
+const path = require('path');   
 
 const logEvents = async (message, logName) => {
     const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
